e59894a3278530275e724f2c72660fd4ac44750d
VijaySammeta <165911306+vijaysammeta@users.noreply.github.com>
2026-09-19 11:00:46 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("HELLO WORLD!!", app.getMessage());
+        assertEquals("Hello World!!", app.getMessage());
     }
 }
